140efca7d7ce6ff2503ef4da9dce77e09154a54d
Hilmi <hilmimuzhaffar15@gmail.com>
2026-07-05 10:25:25 +0700

    feat:add progress on top screen as a loading process

--- frontend/src/components/GuestRoute.jsx
+++ frontend/src/components/GuestRoute.jsx
@@ -1,11 +1,32 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { useEffect } from "react";
+
+NProgress.configure({
+  showSpinner: false,
+  speed: 400,
+  minimum: 0.2,
+});
 
 export function GuestRoute() {
   const { user, loading } = useAuth();
 
+  useEffect(() => {
+    if (loading) {
+      NProgress.start();
+    } else {
+      NProgress.done();
+    }
+
+    return () => {
+      NProgress.done();
+    };
+  }, [loading]);
+
   if (loading) {
-    return <div>Loading...</div>; // Atau komponen loading kustom
+    return null;
   }
 
   return user ? <Navigate to="/dashboard" replace /> : <Outlet />;
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,26 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { useEffect } from "react";
 
 export function ProtectedRoute() {
   const { user, loading } = useAuth();
 
+  useEffect(() => {
+    if (loading) {
+      NProgress.start();
+    } else {
+      NProgress.done();
+    }
+
+    return () => {
+      NProgress.done();
+    };
+  }, [loading]);
+
   if (loading) {
-    return <div>Loading...</div>; // Atau komponen loading kustom
+    return null;
   }
 
   return user ? <Outlet /> : <Navigate to="/auth" replace />;
